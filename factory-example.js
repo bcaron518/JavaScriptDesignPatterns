d6cb6cd844ca6d154cd9cce986e4a3d8064ac463
Benjamin Caron <bcaron@ggc.edu>
2023-11-09 13:27:30 -0500

    Create factory-example.js

---
+++ factory-example.js
@@ -0,0 +1,16 @@
+// factory-example.js
+
+const { CarFactory } = require('./factory');
+
+// Create instances of the CarFactory
+const carFactory = new CarFactory();
+
+// Create different types of cars using the factory
+const sedan = carFactory.createCar('Saloon');
+const suv = carFactory.createCar('SUV');
+const truck = carFactory.createCar('Truck');
+
+// Display the types of cars created
+console.log(saloon.type); // Saloon
+console.log(suv.type);   // SUV
+console.log(truck.type); // Truck
